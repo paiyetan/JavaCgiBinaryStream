020ea63da00923743442b74fc5729fe667608f88
Jianwen Wei <wei.jianwen@gmail.com>
2011-10-15 16:34:56 +0800

    Java cgi class TransportImageOverHttp -- read an image from local filesystem and send it as octet stream via HTTP. - Current statge: Read demo.jpg and write to stdout in byte array.

--- src/cn/edu/sjtu/omnilab/cgi/TransportImageOverHttp.java
+++ src/cn/edu/sjtu/omnilab/cgi/TransportImageOverHttp.java
@@ -3,6 +3,12 @@
  */
 package cn.edu.sjtu.omnilab.cgi;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
 /**
  * @author jianwen
  *
@@ -20,21 +26,27 @@ public class TransportImageOverHttp {
 		System.out.println("Content-type: " + contType);
 	}
 	
-	public void printHttpBody() {
-		System.out.print("<html>");
-		System.out.print("<a href=\"http://www.github.com\">");
-		System.out.print("Click here to github");
-		System.out.print("</a>");
-		System.out.print("</html>");
+	public void printHttpBody() throws IOException {
+		/* Read a file */
+		File imgFile = new File("demo.jpg");
+		int fileSize = (int)imgFile.length();
+		byte[] buffer = new byte[fileSize];
+		FileInputStream fileStream = new FileInputStream(imgFile);
+		fileStream.read(buffer);
+		
+		/* Write to stdout*/
+		PrintStream stdout = new PrintStream(System.out);
+		stdout.write(buffer);
 	}
 	
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		TransportImageOverHttp transDemo = new TransportImageOverHttp();
 		
-		transDemo.printHttpHead("text/html");
+		transDemo.printHttpHead("image/jpeg");
 		System.out.println("");
 		transDemo.printHttpBody();
 	}
